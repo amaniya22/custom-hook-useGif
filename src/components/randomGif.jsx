d192306269b9175f33d6f5de75c968da7c293a42
amaniya22 <amaniyafaizal@gmail.com>
2025-07-18 15:23:04 +0530

    update v3, fixed loading and error for random rendering

--- src/components/randomGif.jsx
+++ src/components/randomGif.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useGif from "../hooks/useGif";
 
 const RandomGif = () => {
-  const { getGIF, fetchGif } = useGif();
+  const { getGIF, fetchGif, loading, error } = useGif();
+  console.log("RandomGif component rendered", getGIF);
+
+  useEffect(() => {
+    fetchGif();
+  }, [])
 
   return (
     <div className="random-gif-container">
       <h1>Random Gif</h1>
       <div className="gif-img-container">
-        <img width="500" src={getGIF} alt="Random Gif" />
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {getGIF && <img width="500" src={getGIF} alt="Random Gif" />}
       </div>
-      <button className="gif-btn-new" onClick={fetchGif}>
-        CLICK FOR NEW
+      <button className="gif-btn-new" onClick={() => fetchGif()}>
+        {loading ? "Fetching..." : "CLICK FOR NEW"}
       </button>
     </div>
   );
